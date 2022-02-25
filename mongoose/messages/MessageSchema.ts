01bf980bbcb6fe3a5caa119cfeb4d23e6078a069
Kulkarni <poornakulkarni2000@gmail.com>
2022-02-24 22:05:52 -0500

    Updated A2

--- mongoose/messages/MessageSchema.ts
+++ mongoose/messages/MessageSchema.ts
@@ -9,6 +9,6 @@ const MessageSchema = new mongoose.Schema<Message>({
     message: {type: String, required: true},
     sentTo: {type: Schema.Types.ObjectId, ref:"UserModel"},
     sentFrom: {type: Schema.Types.ObjectId, ref:"UserModel"},
-    sentOn: Date
+    sentOn: {type: Date, default: Date.now}
 },{collection: "messages"});
 export default MessageSchema;
